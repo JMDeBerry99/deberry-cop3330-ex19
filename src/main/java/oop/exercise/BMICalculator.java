d7232c925558cc90ed229b44b978f801298310c3
Jacob DeBerry <JMDeBerry99@gmail.com>
2021-06-06 21:21:30 -0400

    Adds Scanner functionality

---
+++ src/main/java/oop/exercise/BMICalculator.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jacob DeBerry
+ */
+package oop.exercise;
+import java.util.Scanner;
+
+public class BMICalculator
+{
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner(System.in);
+    }
+}
